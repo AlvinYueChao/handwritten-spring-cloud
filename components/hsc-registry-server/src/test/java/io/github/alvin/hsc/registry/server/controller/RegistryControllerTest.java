450099b92e6fd46ebe57869556aaa84703f2cd36
Yue Chao <yue327305210@163.com>
2025-08-06 12:35:21 +0800

    :bug:fix: fix deprecated @MockBean in test file

--- components/hsc-registry-server/src/test/java/io/github/alvin/hsc/registry/server/controller/RegistryControllerTest.java
+++ components/hsc-registry-server/src/test/java/io/github/alvin/hsc/registry/server/controller/RegistryControllerTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
@@ -36,7 +36,7 @@ class RegistryControllerTest {
     @Autowired
     private WebTestClient webTestClient;
 
-    @MockBean
+    @MockitoBean
     private RegistryService registryService;
 
     private ServiceRegistration testRegistration;
